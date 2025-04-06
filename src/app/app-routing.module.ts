9c76396feb8f895770eb85f844f4505ce2198da6
Marcelo Filipov <marcelo.filipov@gmail.com>
2025-04-05 22:58:45 -0300

    feat: Demais implementações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './features/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+  {
+    path: 'results/:username',
+    loadChildren: () =>
+      import('./features/results/results.module').then((m) => m.ResultsModule),
+  },
+  {
+    path: 'repo/:username/:repoName',
+    loadChildren: () =>
+      import('./features/repo-detail/repo-detail.module').then(
+        (m) => m.RepoDetailModule
+      ),
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  login: string;
+  name: string;
+  bio: string;
+  followers: number;
+  following: number;
+  public_repos: number;
+  location: string;
+  avatar_url: string;
+}
