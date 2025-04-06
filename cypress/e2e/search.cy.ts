e70d6b87243098d38f9b62af84ded00dab495d7d
Marcelo Filipov <marcelo.filipov@gmail.com>
2025-04-06 13:59:27 -0300

    fix: testes e2e

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -2,20 +2,18 @@ describe('Busca de usuários do GitHub', () => {
   it('deve buscar um usuário existente e exibir os dados', () => {
     cy.visit('http://localhost:4200');
 
-    cy.get('input[placeholder="Digite o nome de usuário"]').type('filipovdev');
+    cy.get('#username').type('marcelofilipov'); // mais seguro que usar placeholder
     cy.get('button[type="submit"]').click();
 
-    cy.url().should('include', '/results/filipovdev');
+    cy.url().should('include', '/results/marcelofilipov');
     cy.contains('Repositórios');
-    cy.get('.repo-item').should('have.length.greaterThan', 0);
+    cy.get('[data-cy="repo-item"]').should('have.length.greaterThan', 0);
   });
 
   it('deve exibir mensagem de erro se o usuário não existir', () => {
     cy.visit('http://localhost:4200');
 
-    cy.get('input[placeholder="Digite o nome de usuário"]').type(
-      'usuarioinexistente123456'
-    );
+    cy.get('#username').type('usuarioinexistente123456');
     cy.get('button[type="submit"]').click();
 
     cy.contains('Usuário não encontrado');
